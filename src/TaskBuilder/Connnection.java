c9c9582bd2beb640516d2c055b920e246896da15
GudiVaraprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2020-11-20 00:01:57 +0530

    JDBC Project

---
+++ src/TaskBuilder/Connnection.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TaskBuilder;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author gudiv
+ */
+class Connnection {
+
+    PreparedStatement prepareStatement(String insert_into_todolistTask_NameDescriptionv) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
